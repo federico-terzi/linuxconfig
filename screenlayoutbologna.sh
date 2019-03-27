4d5aed07dc6ae52066eb71d6ab57d9bfd89bab4f
Federico Terzi <federicoterzi96@gmail.com>
2019-03-27 21:22:29 +0100

    New structure

--- screenlayoutbologna.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-1 --mode 1366x768 --pos 0x176 --rotate normal --output HDMI-3 --off --output HDMI-2 --mode 1680x1050 --pos 3286x30 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 1366x0 --rotate normal
