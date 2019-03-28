0f1b37ba6a97df0fa4d43b57c97df9109a98f2a0
Federico Terzi <federicoterzi96@gmail.com>
2019-03-28 18:33:54 +0100

    Fix multimonitor ordering

--- script/screenlayoutbologna.sh
+++ script/screenlayoutbologna.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
 xrandr --output DP-1 --mode 1366x768 --pos 0x176 --rotate normal --output HDMI-3 --off --output HDMI-2 --mode 1680x1050 --pos 3286x30 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 1366x0 --rotate normal
+
+i3-msg workspace 1
+i3-msg move workspace to output DP-1
+i3-msg workspace 2
+i3-msg move workspace to output HDMI-1
+i3-msg workspace 3
+i3-msg move workspace to output HDMI-2
