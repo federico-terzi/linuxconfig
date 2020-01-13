9f56be67c442b4d476c1be88faf42dcb0cf22bad
Federico Terzi <federicoterzi96@gmail.com>
2020-01-13 15:58:19 +0100

    Improve rofi support

--- script/rofi_search.sh
+++ script/rofi_search.sh
@@ -3,11 +3,13 @@
 DIR=".config Desktop Documents Downloads Dropbox linuxconfig insync Music Pictures projects Videos Tamplates 'VirtualBox VMs"
 
 if [ "$1" = "dir" ] ; then
-    find -L $(echo $DIR) -type d,l | rofi -dmenu | xargs xdg-open
+    find -L $(echo $DIR) -type d,l | rofi -i -dmenu | xargs xdg-open
 elif [ "$1" = "vim" ] ; then
-    find -L $(echo $DIR) -type f,l | rofi -dmenu | xargs -I{} xfce4-terminal -e 'vim {}'
+    find -L $(echo $DIR) -type f,l | rofi -i -dmenu | xargs -I{} xfce4-terminal -e 'vim "{}"'
 elif [ "$1" = "docs" ] ; then
-    find ~/linuxconfig/notes/ | rofi -dmenu | xargs -I{} xfce4-terminal -e "zsh -c 'bat {};zsh'"
+    find ~/linuxconfig/notes/ | rofi -i -dmenu | xargs -I{} xfce4-terminal -e "zsh -c 'bat {};zsh'"
 elif [ "$1" = "script" ] ; then
-    find ~/linuxconfig/script/ | rofi -dmenu | xargs -I{} xfce4-terminal -e '{}'
+    find ~/linuxconfig/script/ | rofi -i -dmenu | xargs -I{} xfce4-terminal -e '{}'
+elif [ "$1" = "open" ] ; then
+    find -L $(echo $DIR) -type f,l | rofi -i -dmenu | xargs -I{} xdg-open '{}'
 fi
