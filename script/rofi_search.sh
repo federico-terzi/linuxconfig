023ebe6b18b9d0a4dc4386979461e759f93b0aa4
Federico Terzi <federicoterzi96@gmail.com>
2020-01-19 23:04:07 +0100

    Fix vim opening

--- script/rofi_search.sh
+++ script/rofi_search.sh
@@ -5,7 +5,7 @@ DIR=".config Desktop Documents Downloads Dropbox linuxconfig insync Music Pictur
 if [ "$1" = "dir" ] ; then
     find -L $(echo $DIR) -type d,l | rofi -i -dmenu | xargs xdg-open
 elif [ "$1" = "vim" ] ; then
-    find -L $(echo $DIR) -type f,l | rofi -i -dmenu | xargs -I{} xfce4-terminal -e 'vim "{}"'
+    find -L $(echo $DIR) -type f,l | rofi -i -dmenu | xargs -I{} xfce4-terminal -e 'zsh -c "sleep 0.1 && vim '\''{}'\''"'
 elif [ "$1" = "docs" ] ; then
     find ~/linuxconfig/notes/ | rofi -i -dmenu | xargs -I{} xfce4-terminal -e "zsh -c 'bat {};zsh'"
 elif [ "$1" = "script" ] ; then
