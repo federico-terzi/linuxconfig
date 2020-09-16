e49cc6f0a3ae5d44493538644199a28e0a0d4d13
Federico Terzi <federicoterzi96@gmail.com>
2020-09-16 20:11:24 +0200

    Add vertical monitor configuration

--- script/screenlayoutsangio4.sh
+++ script/screenlayoutsangio4.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-xrandr --output DisplayPort-1 --off --output DisplayPort-0 --off --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1-5 --off --output HDMI-1-4 --off --output HDMI-1-3 --off --output DP-1-3 --off --output HDMI-A-1 --mode 1920x1080 --pos 3840x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 1920x0 --rotate normal
+FIRST=$(xrandr | grep " connected" | awk '{print $1}' | sort | head -1 | tail -1)
+SECOND=$(xrandr | grep " connected" | awk '{print $1}' | sort | head -2 | tail -1)
+THIRD=$(xrandr | grep " connected" | awk '{print $1}' | sort | head -3 | tail -1)
+
+# Vertical monitor
+xrandr --output DisplayPort-1 --off --output DisplayPort-0 --off --output $FIRST --mode 1920x1080 --pos 0x0 --rotate left --output HDMI-1-5 --off --output HDMI-1-4 --off --output HDMI-1-3 --off --output DP-1-3 --off --output $THIRD --mode 1920x1080 --pos 3000x360 --rotate normal --output $SECOND --mode 1920x1080 --pos 1080x360 --rotate normal
+
+# Horizontal monitor
+#xrandr --output DisplayPort-1 --off --output DisplayPort-0 --off --output $FIRST --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1-5 --off --output HDMI-1-4 --off --output HDMI-1-3 --off --output DP-1-3 --off --output $THIRD --mode 1920x1080 --pos 3840x0 --rotate normal --output $SECOND --mode 1920x1080 --pos 1920x0 --rotate normal
 
 i3-msg workspace 1
-i3-msg move workspace to output DVI-D-0
+i3-msg move workspace to output $FIRST 
 i3-msg workspace 2
-i3-msg move workspace to output HDMI-A-0
+i3-msg move workspace to output $SECOND
 i3-msg workspace 3
-i3-msg move workspace to output HDMI-A-1
+i3-msg move workspace to output $THIRD 
 
 
